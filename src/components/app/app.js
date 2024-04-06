899e8764314df046736e26411fb945b1ae643320
LordLye <lye3@mail.ru>
2024-04-06 16:53:00 +0300

    init настраивает реакт приложение

---
+++ src/components/app/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./app.css";
+import PageWrapper from "../../components/layout/page-wrapper/page-wrapper";
+
+export default function App() {
+  return <PageWrapper/>
+}
